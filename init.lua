a0b2a6d35750d2c74bfc9bda5369c3109150dee0
Zareen Price <yazatamorph@gmail.com>
2024-08-07 02:18:54 -0700

    Formatting

--- init.lua
+++ init.lua
@@ -442,10 +442,12 @@ require('lazy').setup({
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
-      -- Manually enable language servers included with language binaries
-      require('lspconfig').erlangls.setup {}
+      -- Manually enable ls included with language binaries
       require('lspconfig').gleam.setup {}
       require('lspconfig').ocamllsp.setup {}
+
+      -- Manually enable ls from AUR
+      require('lspconfig').erlangls.setup {}
 
       -- Enable the following language servers
       local servers = {
